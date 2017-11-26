bef80db91d5a53f043ed3432718ac25d26aab12e
DiegoB <diegob@erp>
2017-11-26 15:26:37 -0300

    Terminada Parte Produccion

--- src/main/PaginaInicial.java
+++ src/main/PaginaInicial.java
@@ -1,6 +1,4 @@
-
 package main;
-
 
 import entidades.CentroTrabajo;
 import entidades.CostoFijo;
@@ -13,47 +11,43 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
-
 public class PaginaInicial extends javax.swing.JFrame {
 
-   Controlador controlador;
-   
+    Controlador controlador;
+
     public PaginaInicial() {
-   
-        try{
-FachadaInterna.getInstance().iniciarTransaccion();
-  
- initComponents();
-        
-        controlador =new Controlador();
+
+        try {
+            FachadaInterna.getInstance().iniciarTransaccion();
+
+            initComponents();
+
+            controlador = new Controlador();
+        } catch (Exception ex) {
+            initComponents();
+
+            controlador = new Controlador();
+
         }
-        
-        catch (Exception ex) {
-             initComponents();
-        
-        controlador =new Controlador();
-            
-        }
-        
-           
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
- 
+
         addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 close();
             }
         });
-   
-  
-}
 
-
-    private void close(){
-        if (jOptionPane1.showConfirmDialog(rootPane, "¿Desea realmente salir del sistema?",
-                "Salir del sistema", jOptionPane1.YES_NO_OPTION) == jOptionPane1.YES_OPTION)
-            System.exit(0);
     }
+
+    private void close() {
+        if (jOptionPane1.showConfirmDialog(rootPane, "¿Desea realmente salir del sistema?",
+                "Salir del sistema", jOptionPane1.YES_NO_OPTION) == jOptionPane1.YES_OPTION) {
+            System.exit(0);
+        }
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -73,6 +67,7 @@ FachadaInterna.getInstance().iniciarTransaccion();
         jLabel2 = new javax.swing.JLabel();
         jButton6 = new javax.swing.JButton();
         jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
 
         jMenu1.setText("jMenu1");
 
@@ -160,62 +155,78 @@ FachadaInterna.getInstance().iniciarTransaccion();
             }
         });
 
+        jButton8.setText("Produccion");
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(MateriaPrima, javax.swing.GroupLayout.DEFAULT_SIZE, 156, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ProductoTermiando, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(200, 200, 200))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(44, 44, 44)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 467, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 467, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jButton3)
-                                .addComponent(MateriaPrima, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jButton6))
-                        .addGap(51, 51, 51)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(ProductoTermiando)
-                                    .addComponent(jButton4))
-                                .addGap(48, 48, 48)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton5)
-                                    .addComponent(jButton2)))
-                            .addComponent(jButton7))))
-                .addContainerGap(46, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(37, 37, 37))
+                .addGap(153, 153, 153))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
-                .addGap(21, 21, 21)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(MateriaPrima)
                     .addComponent(ProductoTermiando)
                     .addComponent(jButton2))
-                .addGap(44, 44, 44)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
                     .addComponent(jButton4)
                     .addComponent(jButton5))
-                .addGap(25, 25, 25)
+                .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
                     .addComponent(jButton6)
-                    .addComponent(jButton7))
+                    .addComponent(jButton7)
+                    .addComponent(jButton8))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
                 .addContainerGap())
         );
 
@@ -242,84 +253,88 @@ FachadaInterna.getInstance().iniciarTransaccion();
     }// </editor-fold>//GEN-END:initComponents
 
     private void MateriaPrimaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MateriaPrimaActionPerformed
-  
+
         List<MateriaPrima> listMateriaPrima = controlador.iniciar();
-       ABMMateriaPrima  Gui_NuevaAbm = new ABMMateriaPrima(controlador, listMateriaPrima);
+        ABMMateriaPrima Gui_NuevaAbm = new ABMMateriaPrima(controlador, listMateriaPrima);
         Gui_NuevaAbm.setVisible(true);
-         dispose();
+        dispose();
     }//GEN-LAST:event_MateriaPrimaActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-         FachadaInterna.getInstance().finalizarTransaccion();
-         dispose();
-         System.exit(0);
+        FachadaInterna.getInstance().finalizarTransaccion();
+        dispose();
+        System.exit(0);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void ProductoTermiandoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ProductoTermiandoActionPerformed
         // TODO add your handling code here:
-        
+
         List<ProductoTerminado> listProductoTerminado = controlador.iniciarProductoTerminado();
-    
-       ABMProductoTerminado  Gui_NuevaAbm = new ABMProductoTerminado(controlador, listProductoTerminado);
+
+        ABMProductoTerminado Gui_NuevaAbm = new ABMProductoTerminado(controlador, listProductoTerminado);
         Gui_NuevaAbm.setVisible(true);
-         dispose();
+        dispose();
     }//GEN-LAST:event_ProductoTermiandoActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        
-            List<ProductoMateriaPrima> listProductoMateriaPrima = controlador.iniciarEstructuraProductoCompleta();
 
-            EstructurasProductos  Gui_NuevaAbm = new EstructurasProductos(controlador, listProductoMateriaPrima);
-            Gui_NuevaAbm.setVisible(true);
-            dispose();
+        List<ProductoMateriaPrima> listProductoMateriaPrima = controlador.iniciarEstructuraProductoCompleta();
+
+        EstructurasProductos Gui_NuevaAbm = new EstructurasProductos(controlador, listProductoMateriaPrima);
+        Gui_NuevaAbm.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        
-               List<CentroTrabajo> listCentroTrabajo = controlador.iniciarCentroTrabajo();
-       ABMCentroTrabajo  Gui_NuevaAbm = new ABMCentroTrabajo(controlador, listCentroTrabajo);
+
+        List<CentroTrabajo> listCentroTrabajo = controlador.iniciarCentroTrabajo();
+        ABMCentroTrabajo Gui_NuevaAbm = new ABMCentroTrabajo(controlador, listCentroTrabajo);
         Gui_NuevaAbm.setVisible(true);
-         dispose();
+        dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-                            List<Maquinaria> listMaquinaria = controlador.iniciarMaquinaria();
-dispose();
- List<CentroTrabajo> listCentroTrabajo = controlador.iniciarCentroTrabajo();
-            ABMMaquinaria  Gui_NuevaAbm = new ABMMaquinaria(controlador, listMaquinaria, listCentroTrabajo);
-            Gui_NuevaAbm.setVisible(true);
+        List<Maquinaria> listMaquinaria = controlador.iniciarMaquinaria();
+        dispose();
+        List<CentroTrabajo> listCentroTrabajo = controlador.iniciarCentroTrabajo();
+        ABMMaquinaria Gui_NuevaAbm = new ABMMaquinaria(controlador, listMaquinaria, listCentroTrabajo);
+        Gui_NuevaAbm.setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
-        
-                
+
         List<ProductoTerminado> listProductoTerminado = controlador.iniciarProductoTerminado();
-    
-       TablaProducto  Gui_NuevaAbm = new TablaProducto(controlador, listProductoTerminado);
+
+        TablaProducto Gui_NuevaAbm = new TablaProducto(controlador, listProductoTerminado);
         Gui_NuevaAbm.setVisible(true);
-         dispose();
+        dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-      List<CostoFijo> listCostoFijo = controlador.iniciarCostoFijo();
-       ABMCostoFijo  Gui_NuevaAbm = new ABMCostoFijo(controlador, listCostoFijo);
+        List<CostoFijo> listCostoFijo = controlador.iniciarCostoFijo();
+        ABMCostoFijo Gui_NuevaAbm = new ABMCostoFijo(controlador, listCostoFijo);
         Gui_NuevaAbm.setVisible(true);
-         dispose();
+        dispose();
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-          List<MateriaPrima> listMateriaPrima = controlador.iniciar();
-       TablaMateria Gui_NuevaAbm = new TablaMateria(controlador, listMateriaPrima);
+        List<MateriaPrima> listMateriaPrima = controlador.iniciar();
+        TablaMateria Gui_NuevaAbm = new TablaMateria(controlador, listMateriaPrima);
         Gui_NuevaAbm.setVisible(true);
-         dispose();     // TODO add your handling code here:
+        dispose();     // TODO add your handling code here:
     }//GEN-LAST:event_jButton7ActionPerformed
 
- 
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        TablaProduccion Gui_NuevaAbm = new TablaProduccion(controlador);
+        Gui_NuevaAbm.setVisible(true);
+        dispose();     // TODO add your handling code here:
+    }//GEN-LAST:event_jButton8ActionPerformed
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -352,15 +367,13 @@ dispose();
         //</editor-fold>
 
         /* Create and display the form */
-      
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
- 
+
                 new PaginaInicial().setVisible(true);
             }
         });
-        
-        
+
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -373,6 +386,7 @@ dispose();
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JDialog jDialog1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
